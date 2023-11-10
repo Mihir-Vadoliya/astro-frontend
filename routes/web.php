09ab0432307fffba72fc72bc5e89cf5c7cd63744
Mihir-Vadoliya <web91.mihir@gmail.com>
2023-11-10 17:47:04 +0530

    add

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,7 @@ use App\Http\Controllers\PaymentController;
 |
 */
 Route::get('/', function (){
-    echo "I knew you would pay me a visit";
+    echo "I knew you would pay me a visits";
 });
 
 Route::get('/checkStatus/{customerId}', [LanderController::class, 'checkStatus']);
